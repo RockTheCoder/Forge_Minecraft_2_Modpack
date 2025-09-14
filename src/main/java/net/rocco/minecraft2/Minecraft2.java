a7054fedc6ac88194798d45cb71e345d694afb8d
RockTheCoder <roccorajewich@gmail.com>
2025-09-13 21:21:15 -0700

    Added Coolguy comment

--- src/main/java/net/rocco/minecraft2/Minecraft2.java
+++ src/main/java/net/rocco/minecraft2/Minecraft2.java
@@ -14,7 +14,7 @@ import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
-
+//coolguy
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(Minecraft2.MOD_ID)
 public class Minecraft2 {
