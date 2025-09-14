fd61f7b55e104da91a0e0530ed824d62335632fa
RockTheCoder <roccorajewich@gmail.com>
2025-09-13 21:50:56 -0700

    Added Ruby

---
+++ src/main/java/net/rocco/minecraft2/item/ModItems.java
@@ -0,0 +1,22 @@
+package net.rocco.minecraft2.item;
+
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import net.rocco.minecraft2.Minecraft2;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, Minecraft2.MOD_ID);
+
+    public static final RegistryObject<Item> RUBY = ITEMS.register("ruby",
+            () -> new Item(new Item.Properties()));
+
+
+
+    public static void register(IEventBus eventBus){
+        ITEMS.register(eventBus);
+    }
+}
